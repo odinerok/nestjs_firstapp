df49d3fcd4f8f22ba4be2ebd74bcd3c7e6ba3f26
odinerok <110151839+odinerok@users.noreply.github.com>
2024-11-08 12:29:06 +0200

    hi message updated

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): {} {
-    const ret = {message: 'Hello World!'};
+    const ret = {message: 'nestjsapi2'};
     return (ret);
   }
 }
